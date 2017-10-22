d8bb887d1e349b54aff5d6965a0e19735d5de22f
Fred Hasselman <me@fredhasselman.com>
2017-10-22 22:55:21 +0200

    add center()

--- R/fRedsRutils.R
+++ R/fRedsRutils.R
@@ -1923,3 +1923,82 @@ scaleR <- function(x,mn=min(x,na.rm=T),mx=max(x,na.rm=T),lo=0,hi=1){
   return(u)
 }
 
+center <- function(numvec, na.rm=TRUE, type = c("mean","median")[1]){
+  switch(type,
+         mean   = centroid <- mean(as.numeric(numvec), na.rm=na.rm),
+         median = centroid <- median(as.numeric(numvec), na.rm=na.rm),
+         )
+  return((as.numeric(numvec) - centroid))
+}
+
+normalise <- function(numvec, na.rm=TRUE){
+  (as.numeric(numvec) - mean(as.numeric(numvec),na.rm=na.rm)) / sd(as.numeric(numvec),na.rm=na.rm)
+}
+
+# Help lme4 get a better convergence
+nlopt <- function(par, fn, lower, upper, control) {
+  # Add to call: control = lmerControl(optimizer = "nloptwrap", calc.derivs = FALSE
+  .nloptr <<- res <- nloptr(par, fn, lb = lower, ub = upper,
+                            opts = list(algorithm = "NLOPT_LN_BOBYQA", print_level = 1,
+                                        maxeval = 1000, xtol_abs = 1e-6, ftol_abs = 1e-6))
+  list(par = res$solution,
+       fval = res$objective,
+       conv = if (res$status > 0) 0 else res$status,
+       message = res$message
+  )
+}
+
+# Convert decimal point
+c2p <- function(text,N=1){
+  if(!is.character(text)){text<-as.character(text)}
+  if(sum(grepl("[,]",text))>=N){text <- gsub(",",".",text)}
+  return(text)
+}
+
+# Count missing values in x
+nmissing <- function(x){
+  sum(is.na(x))
+}
+
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data = NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval = .95, .drop=TRUE) {
+  library(plyr)
+
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=na.rm) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+
+  # Rename the "mean" column
+  #datac <- rename(datac, c("mean" = measurevar))
+
+
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval:
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+
+  return(datac)
+}
